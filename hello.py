64e7e52e47d09476324bd148ff730f678229ec5b
Josue <20175621@ce.pucmm.edu.do>
2022-03-27 13:00:32 -0400

    Mejor que ayer

--- hello.py
+++ hello.py
@@ -7,7 +7,24 @@ from helpers import login_required
 import sqlite3
 
 app = Flask(__name__)
+
+# Asegura que los templates se recargen
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 database = "data.db"
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+# Usa el sistema de archivo (en vez de cookies firmadas)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 @app.route("/")
 def index():
@@ -46,40 +63,63 @@ def movie_info():
 @app.route('/login', methods=["GET", "POST"])
 def login():
 
+    # Olvida cualquier id de usuario
+    session.clear()
+
     if request.method == "POST":
 
         cur = sqlite3.connect(database).cursor()
 
         email = request.form.get('email')
-        password = generate_password_hash(request.form.get('password'))
+        password = request.form.get('password')
+
+        res = cur.execute("SELECT * FROM users WHERE email = ?", (email,)).fetchall()
+
+        if len(res) != 1 or not check_password_hash(res[0][4], password):
+            return render_template("login_apology.html")
+
+        # Remember which user has logged in
+        session["user_id"] = res[0][0]
+        session["email"] = res[0][3]
+
+        # Redirect user to home page
+        return redirect("/")
 
     return render_template('login.html')
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
 
 
 @app.route('/register', methods=["GET", "POST"])
 def register():
 
-    con = sqlite3.connect(database)
-    cur = con.cursor()
-
     if request.method == "POST":
         
-        name = request.form.get('username')
+        first_name = request.form.get('first_name')
+        last_name = request.form.get('last_name')
         email = request.form.get('email')
         password = generate_password_hash(request.form.get('password'))
 
         # Syntactic Sugar
-        guardar_usuario(name, email, password)
+        guardar_usuario(first_name, last_name, email, password)
 
         return redirect(url_for('login'))
 
     return render_template('register.html')
 
-def guardar_usuario(name, email, password):
+def guardar_usuario(first_name, last_name, email, password):
 
     con = sqlite3.connect(database)
     cur = con.cursor()
 
-    cur.execute("INSERT INTO users (username, email, hash) VALUES (?, ?, ?)", (name, email, password))
+    cur.execute("INSERT INTO users (first_name, last_name, email, hash) VALUES (?, ?, ?, ?)", (first_name, last_name, email, password))
     con.commit()
     con.close()
