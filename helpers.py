12c6849f5b48ec014dc0d4824ee2efaba43a26d0
Josue <20175621@ce.pucmm.edu.do>
2022-04-14 15:26:45 -0400

    Agrega funciones para formatear datos de la base de datos

--- helpers.py
+++ helpers.py
@@ -1,4 +1,5 @@
-from flask import redirect, session
+from flask import redirect, session, flash
+from werkzeug.security import check_password_hash, generate_password_hash #sha256
 from functools import wraps
 
 def login_required(f):
@@ -13,3 +14,30 @@ def login_required(f):
             return redirect("/login")
         return f(*args, **kwargs)
     return decorated_function
+
+def admin_required(f):
+    """
+    Decorate routes to require login.
+
+    http://flask.pocoo.org/docs/1.0/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("admin_id") is None:
+            flash('No tienes permisos para acceder a ese recurso')
+            return redirect("/")
+        return f(*args, **kwargs)
+    return decorated_function
+
+def format(l):
+    _str = ""
+    for i in l:
+        _str += f"{i[0]}, "
+    return _str[:len(_str) - 2]
+
+def format_time(time):
+    f_time = time.split()[1]
+    hora = int(f_time[:2])
+    period = "PM" if hora >= 12 else "AM"
+    d_time = f"{hora%12}:{f_time[3:5]} {period}"
+    return d_time
