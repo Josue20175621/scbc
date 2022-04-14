a70b963c697e6e204dd431b78760f157a63a4327
Josue <20175621@ce.pucmm.edu.do>
2022-04-14 15:24:32 -0400

    Agrega soporte para buscar peliculas por nombre de actor y director, soporte para ordenar peliculas por categoria, informacion de la pelicula como actores, directores, duracion, clasificacion, idioma, pais

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
+from ssl import ALERT_DESCRIPTION_DECOMPRESSION_FAILURE
 from flask import Flask, flash, jsonify, redirect, render_template, request, session, url_for
 from flask_session import Session
 from tempfile import mkdtemp
-from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.exceptions import HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash #sha256
-from helpers import login_required
+from helpers import admin_required, format, login_required, format_time
 import sqlite3
 
 app = Flask(__name__)
@@ -28,42 +29,122 @@ Session(app)
 
 @app.route("/")
 def index():
-    
     cur = sqlite3.connect(database).cursor()
+    args = request.args
+    error = None
     
-    movies = cur.execute("SELECT * FROM movies ORDER BY title").fetchall()
+    # Ordena por categoria
+    if args.get("categoria"):
+        query = """SELECT id, titulo, poster FROM peliculas JOIN
+            generos ON generos.idPelicula = peliculas.id
+            WHERE generos.genero = ? """
+        peliculas = cur.execute(query, (args.get("categoria"), )).fetchall()
+    else:
+        peliculas = cur.execute("SELECT id, titulo, poster FROM peliculas ORDER BY titulo").fetchall()
+    
+    # No hay peliculas con esa categoria
+    if len(peliculas) == 0:
+        error = f'No hay peliculas en la categoria {args.get("categoria")}'
 
-    return render_template('index.html', movies=movies)
+    return render_template('index.html', peliculas=peliculas, error=error, categoria=args.get("categoria"))
 
 @app.route('/search', methods=["GET", "POST"])
 def search():
     """ Buscar peliculas """
     cur = sqlite3.connect(database).cursor()
+    results = None
+    error = None
+    args = request.args
 
-    if request.method == "POST":
-        movie_title = request.form.get("title")
+    if args.get("filter") == "director":
+        director = args.get("query")
+        results = cur.execute("""SELECT peliculas.id, titulo, poster FROM
+        peliculas JOIN directores ON peliculas.id = directores.idPelicula JOIN
+        personas ON directores.idPersona = personas.id
+        WHERE nombre LIKE ? GROUP BY titulo""", ('%'+director+'%',)).fetchall()
+        
+        error = f"No se encontraron peliculas con director '{director}'"
 
-        results = cur.execute("SELECT * FROM movies WHERE title LIKE ?", ('%'+movie_title+'%',)).fetchall()
+    elif args.get("filter") == "actor":
+        actor = args.get("query")
+        results = cur.execute("""SELECT peliculas.id, titulo, poster FROM 
+        peliculas JOIN aparece ON peliculas.id = aparece.idPelicula JOIN
+        personas ON aparece.idPersona = personas.id
+        WHERE nombre LIKE ? GROUP BY titulo""", ('%'+actor+'%',)).fetchall()
+        
+        error = f"No se encontraron peliculas con actor '{actor}'"
 
-        # No existen peliculas con ese titulo o que contengan ese titulo
-        if len(results) == 0:
-            return render_template("search_e.html", title=movie_title)
+    elif args.get("filter") == "pelicula":
+        titulo = args.get("query")
+        results = cur.execute("SELECT * FROM peliculas WHERE titulo LIKE ?", ('%'+titulo+'%',)).fetchall()
+        
+        error = f"No se encontraron peliculas con actor '{titulo}'"
 
-        return render_template('searchres.html', results=results)
-    else:
-        return render_template('search.html')
+    if error is not None and len(results) != 0:
+        error = None
 
-@app.route('/movie/<int:id>')
+    return render_template('search.html', error=error, results=results)
+
+@app.route('/movie/<int:id>', methods=["GET", "POST"])
 def movie(id):
     """ Informacion de la pelicula"""
     cur = sqlite3.connect(database).cursor()
+    
+    query = """SELECT titulo, year, sinopsis, poster, duracion, clasificacion, idioma, pais  FROM peliculas JOIN
+                clasificaciones ON peliculas.id = clasificaciones.idPelicula JOIN
+                idiomas ON peliculas.id = idiomas.idPelicula JOIN
+                paises ON peliculas.id = paises.idPelicula JOIN
+                directores ON peliculas.id = directores.idPelicula
+                WHERE peliculas.id = ? GROUP BY titulo"""
+    
+    info_pelicula = cur.execute(query, (id, )).fetchall()
 
-    movies = cur.execute("SELECT * FROM movies WHERE id = ?", (id, )).fetchall()
+    query = """SELECT nombre FROM peliculas JOIN
+                aparece ON peliculas.id = aparece.idPelicula JOIN
+                personas ON aparece.idPersona = personas.id
+                WHERE peliculas.id = ? LIMIT 5"""
+    
+    actores = cur.execute(query, (id, )).fetchall()
 
-    return render_template('movie.html', movies=movies)
+    query = """SELECT nombre FROM peliculas JOIN
+            directores ON peliculas.id = directores.idPelicula JOIN
+            personas ON directores.idPersona = personas.id
+            WHERE peliculas.id = ? """
+
+    directores = cur.execute(query, (id, )).fetchall()
+
+    query = """SELECT idioma FROM peliculas JOIN
+            idiomas ON idiomas.idPelicula = peliculas.id
+            WHERE peliculas.id = ?"""
+    
+    idiomas = cur.execute(query, (id, )).fetchall()
+
+    # Lista las tandas por orden de tiempo de inicio
+    tandas = cur.execute("SELECT * FROM tandas WHERE idPelicula = ? ORDER BY start_time", (id, )).fetchall()
+
+    return render_template('movie.html', info_pelicula=info_pelicula, actores=format(actores), directores=format(directores), idiomas=format(idiomas), tandas=tandas, format_time=format_time)
+
+@app.route('/seats', methods=["GET", "POST"])
+def seats():
+
+    msg = None
+    
+    if request.method == "POST":
+        name = request.form.get('movie_name')
+        tickets = request.form.get('boletos')
+        showing = request.form.get('tanda')
+        msg = 'Selecciona tus asientos'
+    
+    # Usuario no registrado
+    if session.get("user_id") is None:
+        msg = 'Consultar asientos'
+
+    return render_template('seats.html', n_seats=tickets, movie=name, tanda=showing, msg=msg)
 
 @app.route('/login', methods=["GET", "POST"])
 def login():
+
+    error = None
 
     # Olvida cualquier id de usuario
     session.clear()
@@ -75,19 +156,26 @@ def login():
         email = request.form.get('email')
         password = request.form.get('password')
 
-        res = cur.execute("SELECT * FROM users WHERE email = ?", (email,)).fetchall()
+        res = cur.execute("SELECT * FROM usuarios WHERE correo = ?", (email,)).fetchall()
 
+        # El correo no esta en la base de datos o la contraseña no es correcta
         if len(res) != 1 or not check_password_hash(res[0][4], password):
-            return render_template("login_apology.html")
+            error = 'Usuario o contraseña es incorrecto'
+        else:
+            
+            if res[0][5] == 0:
+                # Remember which user has logged in
+                session["user_id"] = res[0][0]
+                session["email"] = res[0][3]
+            else:
+                session["admin_id"] = res[0][0]
+                return redirect(url_for('admin'))
 
-        # Remember which user has logged in
-        session["user_id"] = res[0][0]
-        session["email"] = res[0][3]
+            # Redirecciona al usuario a la pagina principal
+            flash('Has iniciado sesion satisfactoriamente')
+            return redirect(url_for('index'))
 
-        # Redirect user to home page
-        return redirect("/")
-
-    return render_template('login.html')
+    return render_template('login.html', error=error)
 
 @app.route("/logout")
 def logout():
@@ -97,7 +185,7 @@ def logout():
     session.clear()
 
     # Redirect user to login form
-    return redirect("/")
+    return redirect(url_for('login'))
 
 
 @app.route('/register', methods=["GET", "POST"])
@@ -105,30 +193,27 @@ def register():
 
     if request.method == "POST":
         
-        first_name = request.form.get('first_name')
-        last_name = request.form.get('last_name')
-        email = request.form.get('email')
-        password = generate_password_hash(request.form.get('password'))
+        nombre = request.form.get('first_name')
+        apellido = request.form.get('last_name')
+        correo = request.form.get('email')
+        clave = generate_password_hash(request.form.get('password'))
 
-        # Syntactic Sugar
-        guardar_usuario(first_name, last_name, email, password)
+        guardar_usuario(nombre, apellido, correo, clave)
 
         return redirect(url_for('login'))
 
     return render_template('register.html')
 
-@app.route('/buy', methods=["GET", "POST"])
-@login_required
-def buy():
+@app.route('/checkout', methods=["GET", "POST"])
+def checkout():
 
-    return render_template('buy.html')
+    render_template('checkout.html')
 
-
-def guardar_usuario(first_name, last_name, email, password):
+def guardar_usuario(nombre, apellido, correo, clave):
 
     con = sqlite3.connect(database)
     cur = con.cursor()
 
-    cur.execute("INSERT INTO users (first_name, last_name, email, hash) VALUES (?, ?, ?, ?)", (first_name, last_name, email, password))
+    cur.execute("INSERT INTO usuarios (nombre, apellido, correo, hash, tipo) VALUES (?, ?, ?, ?, 0)", (nombre, apellido, correo, clave))
     con.commit()
     con.close()
